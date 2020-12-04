cfb9193f7fdc0dfd95d8992c76b59de7ddc9b994
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-12-03 22:13:28 -0600

    (Add): Model bands

---
+++ models/band-list.js
@@ -0,0 +1,49 @@
+const Band = require('./band');
+
+class BandList {
+  constructor(name) {
+
+    this.bands = [
+      new Band('Banda Ms'),
+      new Band('Arrolladora'),
+      new Band('Adictiva'),
+      new Band('El recodo'),
+    ];
+
+  };
+
+  addBnad(name) {
+    const newBand = new Band(name);
+    this.bands.push(newBand);
+    return this.bands;
+  };
+
+  removeBand(id) {
+    this.bands.filter(band => band.id !== id);
+  };
+
+  getBands() {
+    return this.bands;
+  };
+
+  increaseVotes(id) {
+    this.bands = this.bands.map(band => {
+      if(band.id === id) {
+        band.votes += 1;
+      };
+      return band;
+    });
+  };
+
+  changeName(id, newName) {
+    this.bands = this.bands.map(band => {
+      if(band.id === id) {
+        band.name = newName;
+      };
+      return band;
+    });
+  };
+
+};
+
+module.exports = Band;
