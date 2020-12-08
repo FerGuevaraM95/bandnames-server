6ddf5a643a6a9af5200e3e5480e3bf2e6f82f852
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-12-07 23:43:02 -0600

    (Add): Configurate socket.io in react

--- bandnames/src/App.js
+++ bandnames/src/App.js
@@ -1,19 +1,52 @@
-import { BandList } from './components/BandList';
-import { BandAdd } from './components/BandAdd';
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+import { BandList } from "./components/BandList";
+import { BandAdd } from "./components/BandAdd";
+
+const connectSocketServer = () => {
+  const socket = io.connect("http://localhost:8080", {
+    transports: ["websocket"],
+  });
+  return socket;
+};
 
 function App() {
+  const [socket] = useState(connectSocketServer());
+  const [online, setOnline] = useState(false);
+
+  useEffect(() => {
+    console.log(socket);
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnline(true);
+    })
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline(false);
+    })
+  }, [socket]);
+
   return (
     <div className="container">
       <div className="alert">
         <p>
           Service status:
-          <span className="text-success"> Online</span>
-          <span className="text-danger"> Offline</span>
+          {online ? (
+            <span className="text-success"> Online</span>
+          ) : (
+            <span className="text-danger"> Offline</span>
+          )}
         </p>
       </div>
 
       <h1>BandNames</h1>
-      <hr/>
+      <hr />
 
       <div className="row">
         <div className="col-8">
@@ -23,7 +56,6 @@ function App() {
           <BandAdd />
         </div>
       </div>
-
     </div>
   );
 }
